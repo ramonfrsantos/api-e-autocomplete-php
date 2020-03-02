64c7079a4e04648729290ef298ddc8ff02efc568
Ramon <ramon_r_santos@hotmail.com>
2020-03-02 17:54:33 -0300

    criação de variável para alteração de id

--- updateUser.php
+++ updateUser.php
@@ -3,18 +3,20 @@
 $conn = new PDO("mysql:host=localhost;dbname=data_base","root","");
 
 // Dados a serem alterados
-$id = 10;
-$login = "João";
-$password = "54321";
-$cadastro = "115112";
+$idantigo = 15;
+$idnovo = 11;
+$login = "Eduarda";
+$password = "44444";
+$cadastro = "505050";
 
 // Modificando usuário no banco
-$updateUser = $conn->prepare("UPDATE tb_usuarios SET deslogin = :LGIN, dessenha = :PASS, dtcadastro = :CADASTRO WHERE idusuario = :ID");
+$updateUser = $conn->prepare("UPDATE tb_usuarios SET idusuario = :IDNOVO, deslogin = :LGIN, dessenha = :PASS, dtcadastro = :CADASTRO WHERE idusuario = :IDANTIGO");
 
 $updateUser->bindParam(":LGIN",$login);
 $updateUser->bindParam(":PASS",$password);
 $updateUser->bindParam(":CADASTRO",$cadastro);
-$updateUser->bindParam(":ID",$id);
+$updateUser->bindParam(":IDNOVO",$idnovo);
+$updateUser->bindParam(":IDANTIGO",$idantigo);
 
 // Executando o comando desejado
 $updateUser->execute();
